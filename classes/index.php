43111fba599ac53632c5a691bef559933597fcd7
Steven Jackson <stevenpjackson0@gmail.com>
2019-11-11 19:42:55 -0600

    Add: Properties to Recipe class

--- classes/index.php
+++ classes/index.php
@@ -3,6 +3,13 @@
 //Create Class
 class Recipe
 {
+    //Add property to our recipe class
+    public $title;
+    public $ingredients =array();
+    public $instructions = array();
+    public $yeild;
+    public $tag = array();
+    public $source = "Alina Holligan";
 
 }
 //Instantiate object and store in variable
