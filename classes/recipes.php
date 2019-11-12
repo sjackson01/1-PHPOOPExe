4153c4c1edaae9713264be16c659212caf8039ac
Steven Jackson <stevenpjackson0@gmail.com>
2019-11-12 13:46:26 -0600

    Add: Foreach that displays values in recipe1 object

--- classes/recipes.php
+++ classes/recipes.php
@@ -66,22 +66,5 @@ class Recipe
     }
 
 }
-//Instantiate object
-$recipe1 = new Recipe();
-$recipe1->source = "Grandma Holligan ";
-//Call setTitle and pass in lowercase title 
-$recipe1->setTitle("my first recipe ");
-$recipe1->addInggredient("egg", 1);
-
-
-//Instantiate second object
-$recipe2 = new Recipe();
-$recipe2->source = "Bettery Crocker";
-$recipe2->setTitle("my second recipe");
-
-//Call display recipe method on both recipe objects
-echo $recipe1->getTitle();
-echo $recipe1->displayRecipe();
-echo $recipe2->displayRecipe();
 
 ?>
--- index.php
+++ index.php
@@ -7,6 +7,7 @@ $recipe1->source = "Grandma Holligan ";
 //Call setTitle and pass in lowercase title 
 $recipe1->setTitle("my first recipe ");
 $recipe1->addInggredient("egg", 1);
+$recipe1->addInggredient("flour", 2, "cup");
 
 
 //Instantiate second object
@@ -14,9 +15,14 @@ $recipe2 = new Recipe();
 $recipe2->source = "Bettery Crocker";
 $recipe2->setTitle("my second recipe");
 
-//Call display recipe method on both recipe objects
+
 echo $recipe1->getTitle();
-echo $recipe1->displayRecipe();
-echo $recipe2->displayRecipe();
+//Display values stored in $recipe1 object
+foreach ($recipe1->getIngredients() as $ing){
+    echo "\n" 
+    . $ing["amount"] . " " 
+    . $ing["measure"] . " " 
+    . $ing["item"];
+}
 
 ?>
