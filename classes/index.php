096911a225dc663cf492207372d6f4f6a24f0a7f
Steven Jackson <stevenpjackson0@gmail.com>
2019-11-12 13:10:11 -0600

    Add: Add ingredient method

--- classes/index.php
+++ classes/index.php
@@ -11,6 +11,18 @@ class Recipe
     public $tag = array();
     public $source = "Alina Holligan";
 
+    //Define measurments
+    private $measurements = array( 
+        "tsp",
+        "tbsp",
+        "cup",
+        "oz",
+        "fl oz",
+        "pint",
+        "quart",
+        "gallon"
+    );
+
     //Add $title setter
     public function setTitle($title)
     {
@@ -21,6 +33,25 @@ class Recipe
     public function getTitle()
     {
         return $this->title;      
+    }
+
+    //Add ingredient method
+    public function addInggredient($item, $amount = null, $measure = null)
+    {   
+        //if $amount != null !=float !=int exit and display type and user input
+        if($amount != null && !is_float($amount) && !is_int($amount)){
+            exit("The amount must be a float: " . gettype($amount) . " $amount given");
+        }
+        //if $measure != null != values in array $measure exit display values
+        if($measure != null && !in_array(strtolower($measure), $this->measurements)){
+            exit("Please enter a valid measurement: " . implode(",", $this->measurements));
+        }
+        //Add $item, $amount, $measure to array
+        $this->ingredients[] = array(
+            "item" => ucwords($item),
+            "amount" => $amount,
+            "measure" => strtolower($measure),
+        );
     }
 
     //Create functiont hat will display recipe title and source
@@ -35,6 +66,7 @@ $recipe1 = new Recipe();
 $recipe1->source = "Grandma Holligan ";
 //Call setTitle and pass in lowercase title 
 $recipe1->setTitle("my first recipe ");
+$recipe1->addInggredient("egg", 1);
 
 
 //Instantiate second object
